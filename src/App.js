feffca0c81a88234856c45144b608d10888df082
jeehyun <cjh7652@hanmail.net>
2023-02-03 11:04:22 +0900

    react-router-dom path

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@
 import './App.css'
 import React from 'react';
 import MainPage from './components/MainPage';
+import {Route, Routes} from 'react-router-dom';
+import ProductPage from './components/ProductPage';
+import UploadPage from './components/UploadPage';
 
 function App() {
 
 
   return(
     <div className='App'>
-      <MainPage />
+        <Routes>
+            <Route path="/" element={<MainPage />}></Route>
+            <Route path="/ProductPage/:id" element={<ProductPage />}></Route>
+            <Route path="/UploadPage" element={<UploadPage />}></Route>
+        </Routes>
+       
     </div>
   )
 }
